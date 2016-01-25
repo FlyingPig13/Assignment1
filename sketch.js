40191a51760293c76b4626f808f6cd6767803af4
FlyingPig13 <anotherhale.m@gmail.com>
2016-01-25 00:29:35 -0500

    Finished
    Solved the issue, was placing the variables for rect in as (width,
    height, xposn, yposn) instead of (xposn, yposn, width height)

--- sketch.js
+++ sketch.js
@@ -18,28 +18,26 @@
 */
 
 function setup() {
-  createCanvas(500, 500)
+  createCanvas(500, 700)
   background(255, 255, 255)
  
 }
 
 function draw() {
+  fill("red")
+  strokeWeight(3)
+  rect(0, 0, 25, 25)
   
   fill("blue")
   strokeWeight(3)
-  //gridRect is a function that takes in 
-   var gridRect = function(x, y, i){
-     
-     
-   }
-   
+  
    //gridBreak determines if the screen has been filled with squares
    //it takes in two numbers:
    //x (the x position of the square) and
    //y (the y position of the squares)
    
    var gridBreak = function(x, y){
-     (x >= width && y>= height)
+     (x >= width && y >= height)
      
    };
    //gridDraw takes in three numbers and outputs a grid of squares
@@ -48,22 +46,17 @@ function draw() {
   //is an integer used to determine if the grid is completed
   
   var gridDraw = function(x, y){
-    while (!gridBreak(x, y)
-    while (x <= width){
-      rect(width/20, height/20, x, y)
-      gridDraw(x+width/20, y)
+    while (!gridBreak(x, y)){
+     while (x <= width){
+      rect(x, y, height/20, height/20)
+      gridDraw(x + height/20, y)
+       
+     }
       gridDraw(0, y+height/20)
-    }
     }
   }
   
-   
- 
- 
+  gridDraw(0, 0)
   
   
-  
-  
-  
-
 }
